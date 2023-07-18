f8d9cf866a95fec1f59c55944de7e9de1db08419
Kayla <81789912+Nechja@users.noreply.github.com>
2023-07-18 01:10:06 -0700

    Update DrinkRepositoryNoFactoryTests.cs

--- DataAccessTests/Services/DrinkRepositoryNoFactoryTests.cs
+++ DataAccessTests/Services/DrinkRepositoryNoFactoryTests.cs
@@ -10,23 +10,5 @@ using DataAccess.Models;
 
 namespace DataAccess.Services.Tests
 {
-	[TestClass()]
-	public class DrinkRepositoryNoFactoryTests
-	{
-		private DrinkDataModel _drink;
 
-		[TestMethod()]
-		public void DrinkRepositoryNoFactoryTest()
-		{
-			DrinkDBContext dbContext = new DrinkDBContext();
-			try
-			{
-				DrinkRepositoryNoFactory repo = new DrinkRepositoryNoFactory(dbContext);
-			}
-			catch (Exception ex)
-			{
-				Assert.Fail(ex.Message);
-			}
-		}
-	}
 }
